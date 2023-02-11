16e80fa3fb9a5901b01ca5794677ea29c9df89bb
svgonline@ya.ru <svgonline@ya.ru>
2023-02-11 20:33:23 +0500

    -s

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './index.css';
+import React, {useState} from 'react';
+import MainForm from './components/MainForm';
+
+function App() {
+  const [form, setForm] = useState({
+    hex: '',
+    rgb: ''
+  })
+
+  const hexToRGB = (hex) => {
+    if (!hex.match(/^#[a-fA-F\d]{6}$/))
+      return "Ошибка в значении"
+    hex = '0x' + hex.slice(1)
+    let r = (hex >> 16) & 0xFF
+    let g = (hex >> 8) & 0xFF
+    let b = hex & 0xFF
+    return `rgb(${r}, ${g}, ${b})`
+  }
+
+  const handleHexChange = e => {
+    const hex = e.target.value
+    setForm(prevForm => ({...prevForm,
+      hex: hex,
+      rgb: hex.length === 7 ? hexToRGB(hex) : prevForm.rgb
+    }));
+  }
+  return (
+    <div className="app" style={{background:form.rgb}}>
+      <MainForm onHexChange={handleHexChange} form={form}/>
+    </div>
+  )
+}
+
+export default App;
